442e2379c2f992456231d17683fa41822820e68a
Keitaro <neoterize@mac.com>
2018-04-25 15:54:58 +0100

    Initial Commit

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Keitaro Takahashi on 2018/04/25.
+//  Copyright © 2018 Keitaro Takahashi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
